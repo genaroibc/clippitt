1a109ebc462ad5a1aece3c1188941b00ee413d8a
genaroibc <bonavitaismael@gmail.com>
2023-03-05 10:16:31 -0300

    [DEV]: configure eslint and prettier

---
+++ .prettierrc.cjs
@@ -0,0 +1,16 @@
+module.exports = {
+  extends: [
+    "eslint:recommended",
+    "plugin:react/recommended",
+    "plugin:import/recommended",
+    "plugin:jsx-a11y/recommended",
+    "plugin:@typescript-eslint/recommended",
+    // This disables the formatting rules in ESLint that Prettier is going to be responsible for handling.
+    // Make sure it's always the last config, so it gets the chance to override other configs.
+    "eslint-config-prettier",
+  ],
+  rules: {
+    // Add your own rules here to override ones from the extended configs.
+    "react/react-in-jsx-scope": "off",
+  },
+};
