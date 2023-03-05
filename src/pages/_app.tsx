2458137623397dc22fcb20957fed993d90e95683
genaroibc <bonavitaismael@gmail.com>
2023-03-05 10:19:03 -0300

    [FEAT]: add Syne font family and SemanticHead to main page

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -1,6 +1,13 @@
-import '@/styles/globals.css'
-import type { AppProps } from 'next/app'
+import "@/styles/globals.css";
+import type { AppProps } from "next/app";
+import { Syne } from "next/font/google";
+
+const syne = Syne({ subsets: ["latin"] });
 
 export default function App({ Component, pageProps }: AppProps) {
-  return <Component {...pageProps} />
+  return (
+    <div className={syne.className}>
+      <Component {...pageProps} />
+    </div>
+  );
 }
