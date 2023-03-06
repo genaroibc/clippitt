74d37eb8e7cf967529ab302edad3d3423ae85526
genaroibc <bonavitaismael@gmail.com>
2023-03-06 13:28:17 -0300

    [DEV]: add types for video cropping

--- src/types/index.ts
+++ src/types/index.ts
@@ -11,7 +11,23 @@ export interface KnownResult<T> {
 export type KnownResponse<T> = KnownResult<T> | KnownError;
 
 export type VideoData = { src: string; title: string };
+export type VideoURL = string;
 
 export type ClipURL = string;
 export type ClipID = string;
 export type ClipSourceURL = string;
+
+export type Size = {
+  width: number;
+  height: number;
+};
+
+export type Coords = {
+  x: number;
+  y: number;
+};
+
+export type VideoConfig = {
+  camera: { coords: Coords; size: Size };
+  content: { coords: Coords; size: Size };
+};
