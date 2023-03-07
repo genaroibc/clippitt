d54bce627a0bfea83296fa186b895866b1dd0442
genaroibc <bonavitaismael@gmail.com>
2023-03-07 15:14:08 -0300

    [FEAT]: style download video section

--- src/components/ChooseLayouts.tsx
+++ src/components/ChooseLayouts.tsx
@@ -14,18 +14,18 @@ type Props = {
 
 export function ChooseLayouts({ onNewLayout }: Props) {
   return (
-    <section className="py-8">
-      <h4 className="text-2xl mb-4 font-bold text-center">Choose a layout</h4>
+    <section>
+      <h4 className="text-2xl mb-8 font-bold text-center">Choose a layout</h4>
 
-      <div className="flex justify-center gap-4">
+      <div className="flex justify-center gap-8">
         {LAYOUTS.map((layout) => (
           <button
-            className="p-0 hover:scale-110 transition-transform"
+            className="p-0 hover:scale-105 hover:shadow-2xl transition-transform"
             key={layout.id}
             onClick={() => onNewLayout(layout.layout)}
           >
             <Image
-              className="max-w-sm"
+              className="max-w-sm rounded"
               width={200}
               height={200}
               src={layout.url}
