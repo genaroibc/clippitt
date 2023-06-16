a55cfe3c9573639fd7cfb6c08a04ae91c9bf65a2
genaroibc <bonavitaismael@gmail.com>
2023-06-16 13:13:59 -0300

    [FEAT]: add hero animation

--- src/components/Hero.tsx
+++ src/components/Hero.tsx
@@ -2,10 +2,21 @@ export function Hero() {
   return (
     <h1 className="text-center font-bold text-3xl md:text-5xl md:max-w-2xl mx-auto my-6">
       Convert your{" "}
-      <span className="leading-relaxed text-violet-700">Twitch</span> Clips to{" "}
-      <span className="leading-relaxed">TikToks</span>,{" "}
-      <span className="leading-relaxed text-pink-600">Reels</span> and{" "}
-      <span className="leading-relaxed text-red-600">Youtube shorts</span>{" "}
+      <span className="leading-relaxed inline-block animate-shine hover:brightness-125 transition-colors duration-300 hover:[text-shadow:2px_2px_20px_currentColor] text-violet-700">
+        Twitch
+      </span>{" "}
+      Clips to{" "}
+      <span className="leading-relaxed inline-block animate-shine hover:brightness-125 transition-colors duration-300 hover:[text-shadow:2px_2px_20px_currentColor]">
+        TikToks
+      </span>
+      ,{" "}
+      <span className="leading-relaxed inline-block animate-shine hover:brightness-125 transition-colors duration-300 hover:[text-shadow:2px_2px_20px_currentColor] text-pink-600">
+        Reels
+      </span>{" "}
+      and{" "}
+      <span className="leading-relaxed inline-block animate-shine hover:brightness-125 transition-colors duration-300 hover:[text-shadow:2px_2px_20px_currentColor] text-red-600">
+        Youtube shorts
+      </span>{" "}
     </h1>
   );
 }
