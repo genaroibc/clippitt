f95d816c8542d27f7cc5e96a89f9e3f76ad8c5cb
genaroibc <bonavitaismael@gmail.com>
2023-11-06 14:23:31 -0300

    feat: change footer description and link

--- src/components/Footer.tsx
+++ src/components/Footer.tsx
@@ -3,12 +3,11 @@ export function Footer() {
     <footer className="border-t-2 py-6 md:px-12 text-center w-full md:max-w-fit mx-auto">
       <p>
         Developed with ❤️ by{" "}
-        <span className="text-violet-500 font-bold">Genaro Bonavita</span> in
+        <span className="text-violet-400">Genaro Bonavita</span> in
         2023
       </p>
       <p>
-        Do you like the project? Meet the author:{" "}
-        <a href="https://genarobonavita.dev">genarobonavita.dev</a>
+        <a href="https://gena.dev">gena.dev</a>
       </p>
     </footer>
   );
