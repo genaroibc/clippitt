8097d046e178419e9e4e5f7c4e060686a563ad68
genaroibc <bonavitaismael@gmail.com>
2024-03-04 12:33:51 -0300

    feat: add umami analytics

--- src/pages/_document.tsx
+++ src/pages/_document.tsx
@@ -3,7 +3,10 @@ import { Html, Head, Main, NextScript } from 'next/document'
 export default function Document() {
   return (
     <Html lang="en">
-      <Head />
+      <Head>
+        {/* Umami Analytics script */}
+        <script defer src="https://us.umami.is/script.js" data-website-id="b43029a9-b512-4a2a-b937-8d818766bbb9"></script>
+      </Head>
       <body>
         <Main />
         <NextScript />
