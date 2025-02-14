9cf55a5778a16043deab01c9b085786af6bfe640
genaroibc <bonavitaismael@gmail.com>
2025-02-14 00:16:16 -0300

    chore: update clips

--- src/constants/demo-clips.ts
+++ src/constants/demo-clips.ts
@@ -5,8 +5,4 @@ export const DEMO_CLIP_URLS = [
     id: uuid(),
     url: "https://www.twitch.tv/ibai/clip/VivaciousSoftScorpionTheTarFu-A5AMqyAB0ta9BZ_K",
   },
-  {
-    id: uuid(),
-    url: "https://www.twitch.tv/midudev/clip/EntertainingWanderingCobblerPrimeMe-IIGNaG5tQac4crV1",
-  },
 ];
